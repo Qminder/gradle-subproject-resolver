4c63c13ce7bf1c33f16153ddedb0f74a5c7046f3
Markus Loide <markus@qminder.com>
2020-05-26 11:13:54 +0300

    tests for parsing gradle

---
+++ gradleparser_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+const newGradleFormatKt = `
+dependencies {
+	implementation(project(":cool-beans"))
+	api(project(":uncool-beans"))
+	implementation("com.beans.cool")
+	api("org.beans.cooler")
+}
+`
+
+const newGradleFormat = `
+dependencies {
+	implementation 'stuff'
+	api "org.beans.refried:{beansVersion}"
+	implementation project(':pinto-beans')
+	api project(':kidney-beans')
+}
+`
+
+const oldGradleFormat = `
+dependencies {
+	compile project(':pinto-beans')
+	runtime project(':kidney-beans')
+}
+`
+
+func TestParser(t *testing.T) {
+	cases := []struct {
+		in   map[string]string
+		want map[string][]string
+	}{
+		{
+			map[string]string{
+				"alpha": newGradleFormatKt,
+			},
+			map[string][]string{
+				"alpha": {"cool-beans", "uncool-beans"},
+			},
+		},
+		{
+			map[string]string{
+				"alpha": newGradleFormat,
+			},
+			map[string][]string{
+				"alpha": {"pinto-beans", "kidney-beans"},
+			},
+		},
+		{
+			map[string]string{
+				"alpha": oldGradleFormat,
+			},
+			map[string][]string{
+				"alpha": {"pinto-beans", "kidney-beans"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		got := CreateDependencyMap(c.in)
+
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("CreateDependencyMap(%v) = %v; want %v", c.in, got, c.want)
+		}
+	}
+}
