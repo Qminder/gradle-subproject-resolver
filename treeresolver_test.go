f623427a63633ff0cc30321bf9fedbfd25bf7890
Markus Loide <markus@qminder.com>
2020-05-26 10:36:39 +0300

    tests for tree resolver

---
+++ treeresolver_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type InputData struct {
+	dependencies  map[string][]string
+	targetproject string
+}
+
+func TestResolver(t *testing.T) {
+	cases := []struct {
+		in   InputData
+		want []string
+	}{
+		{ // No dependencies, nothing to resolve
+			InputData{map[string][]string{}, "alpha"},
+			[]string{},
+		},
+		{ // Project itself is resolved if it exists
+			InputData{map[string][]string{
+				"alpha": {},
+			}, "alpha"},
+			[]string{"alpha"},
+		},
+		{ // Dependencies that do not exist won't be resolved
+			InputData{map[string][]string{
+				"alpha": {"beta"},
+			}, "alpha"},
+			[]string{"alpha"},
+		},
+		{ // Dependencies that have no further dependencies will still be included
+			InputData{map[string][]string{
+				"alpha": {"beta"},
+				"beta":  {"gamma"},
+				"gamma": {},
+			}, "alpha"},
+			[]string{"alpha", "beta", "gamma"},
+		},
+		{ // Starting from a project in the middle of the tree, it only resolves relevant projects
+			InputData{map[string][]string{
+				"alpha": {"beta"},
+				"beta":  {"gamma"},
+				"gamma": {},
+			}, "beta"},
+			[]string{"beta", "gamma"},
+		},
+		{ // Side parts that end up in the same node as we find are not included
+			InputData{map[string][]string{
+				"alpha": {"beta"},
+				"beta":  {"gamma"},
+				"gamma": {},
+				"delta": {"gamma"},
+			}, "alpha",
+			}, []string{"alpha", "beta", "gamma"},
+		},
+		{ // Irrelevant side trees are not included
+			InputData{map[string][]string{
+				"alpha":   {"beta"},
+				"beta":    {"gamma"},
+				"gamma":   {},
+				"delta":   {"epsilon"},
+				"epsilon": {},
+			}, "beta"},
+			[]string{"beta", "gamma"},
+		},
+	}
+
+	for _, c := range cases {
+		fmt.Println(c.in)
+		got := ResolveDependencies(c.in.dependencies, c.in.targetproject)
+		fmt.Println(got)
+		fmt.Println(c.want)
+
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("ResolveDependencies(%v, %s) = %v; want %v", c.in.dependencies, c.in.targetproject, got, c.want)
+		}
+	}
+}
