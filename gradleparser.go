b21738a86241ff26b1b896b26470f7d418e7c4c9
Madis Liias <madisliias@gmail.com>
2025-03-26 15:31:18 +0200

    feat: allow digits in gradle module names (#9)

--- gradleparser.go
+++ gradleparser.go
@@ -2,7 +2,7 @@ package main
 
 import "regexp"
 
-var projectRegex = regexp.MustCompile(`project\(['"]:?([a-z-?A-Z]+)['"]\)`)
+var projectRegex = regexp.MustCompile(`project\(['"]:?([\w-]+)['"]\)`)
 
 func CreateDependencyMap(gradleFiles map[string]string) map[string][]string {
 	dependencyMap := make(map[string][]string)
